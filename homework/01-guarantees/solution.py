d759849e391f766c4af7eb485dc1fb32e64af08d
eugenedar <evgenedar@mail.ru>
2023-09-10 00:11:24 +0300

    partially EO

--- homework/01-guarantees/solution.py
+++ homework/01-guarantees/solution.py
@@ -122,23 +122,42 @@ class ExactlyOnceSender(Process):
     def __init__(self, proc_id: str, receiver_id: str):
         self._id = proc_id
         self._receiver = receiver_id
+        self._need_send = dict()  # {id: msg}
+        self._index = 1
 
     def on_local_message(self, msg: Message, ctx: Context):
-        # receive message for delivery from local user
-        pass
+        # receive message for delivery from local user\
+
+        self._need_send[self._index] = msg
+        msg['id'] = self._index
+
+        self._index += 1
+
+        if len(self._need_send) == 1:
+            ctx.set_timer('resend', 5)
 
     def on_message(self, msg: Message, sender: str, ctx: Context):
         # process messages from receiver here
-        pass
+        if msg['id'] in self._need_send:
+            self._need_send.pop(msg['id'])
+        if len(self._need_send) == 0:
+            ctx.cancel_timer('resend')
 
     def on_timer(self, timer_name: str, ctx: Context):
         # process fired timers here
-        pass
+        if timer_name in {'resend'}:
+            for index in self._need_send.keys():
+                msg = self._need_send[index]
+                msg['id'] = index
+                ctx.send(msg, self._receiver)
+            if len(self._need_send) != 0:
+                ctx.set_timer('resend', 5)
 
 
 class ExactlyOnceReceiver(Process):
     def __init__(self, proc_id: str):
         self._id = proc_id
+        self._queue = []
 
     def on_local_message(self, msg: Message, ctx: Context):
         # not used in this task
@@ -147,7 +166,20 @@ class ExactlyOnceReceiver(Process):
     def on_message(self, msg: Message, sender: str, ctx: Context):
         # process messages from receiver
         # deliver message to local user with ctx.send_local()
-        pass
+        received_index = msg['id']
+        msg.remove('id')
+
+        ctx.send(Message('', {'id': received_index}), sender)
+
+        for index in self._queue:
+            if index == received_index:
+                return
+
+        if len(self._queue) >= 16:
+            self._queue.pop(0)
+        self._queue.append(received_index)
+
+        ctx.send_local(msg)
 
     def on_timer(self, timer_name: str, ctx: Context):
         # process fired timers here
