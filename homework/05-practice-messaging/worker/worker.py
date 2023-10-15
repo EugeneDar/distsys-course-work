1bbd68cb3a67f2f214ba9c3e8cb679df5dd9438b
Eugene Darashkevich <evgenedar@mail.ru>
2023-10-15 14:06:26 +0300

    test_post_image

--- homework/05-practice-messaging/worker/worker.py
+++ homework/05-practice-messaging/worker/worker.py
@@ -1,7 +1,24 @@
 from caption import get_image_caption
+import pika
+import time
 
 
 if __name__ == '__main__':
-    #TODO: Your code here.
-    # Register consumer and implement message processing.
-    raise NotImplementedError
+    connection = pika.BlockingConnection(pika.ConnectionParameters(host='rabbitmq', port=5672))
+    channel = connection.channel()
+
+    channel.queue_declare(queue='task_queue', durable=True)  # to make sure that the queue will survive a RabbitMQ node restart
+    print(' [*] Waiting for messages.')
+
+    def callback(ch, method, properties, body):
+        print(" [x] Received %r" % body.decode())
+        print(ch, method, properties)
+        time.sleep(body.count(b'.'))
+        print(" [x] Done")
+        ch.basic_ack(delivery_tag=method.delivery_tag)  # Send ack
+
+
+    channel.basic_qos(prefetch_count=1)  # This uses the basic.qos protocol method to tell RabbitMQ not to give more than one message to a worker at a time
+    channel.basic_consume(queue='task_queue', on_message_callback=callback)
+
+    channel.start_consuming()
